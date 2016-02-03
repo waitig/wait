d2489132ff956b85c4e19984d1f8b2e2098638c6
waitig <waitig@hotmail.com>
2016-02-03 18:32:10 +0800

    Signed-off-by: waitig <waitig@hotmail.com>

--- js/studio.js
+++ js/studio.js
@@ -7,28 +7,27 @@ $(function() {
 <!-- 首页超大banner切换效果 -->
 $(function(){
 	var index = 0;
-	var imgWidth = $('.pics_switch .pic_box').width();//单张图片宽度
+	var imgWidth = $('.pics_switch').width();//单张图片宽度
 	//alert(imgWidth);
-	var len = $('.pics_switch .pic_box').length;//图片数
+	//alert($(window).width());
+	var len = $('.pic_box').length;//图片数
 	//alert(len)
 	var totalImgWidth = imgWidth*len;//图片序列宽度
-	
 	//默认banne宽度是1600，低于1600的分辨率会出现位置偏差，需要调整，故在低于1600下设置banner宽度为100%；
 	if($(window).width()<imgWidth) {
 			//去掉.ps_box 下3行
 		$('.pics_switch').css({'width':$(window).width()});
-		$('.pics_switch .pic_box').css({'width':$(window).width()});
+		$('.pics_switch .pb .pic_box').css({'width':$(window).width()});
 		$('.pics_switch .pic_box a').css({'width':$(window).width()});
 		imgWidth = $(window).width();
 	}
-	
+	$('.pics_switch .pb .pic_box').css({'width':$('.pics_switch').width()});
 	//小按钮鼠标滑过透明度
 	//该ps_box-.pics_switch 下两行
 	$('.pics_switch .pics_switch_clients ul li').css({'opacity':0.9});
 	$('.pics_switch .pics_switch_clients ul li span.current').css({'opacity':0.9});
 	$('.pics_switch_clients li').hover(function() {
 			$(this).addClass('hover');
-
 		},function() {
 			$(this).removeClass('hover');
 		}
@@ -37,7 +36,7 @@ $(function(){
 	//左右翻页按钮滑过透明度
 	$('.pics_switch .viewArrows').css({'opacity':0.4});
 	$('.pics_switch .viewArrows').hover(function() {
-			$(this).fadeTo(200, 1);
+			$(this).fadeTo(200, 0);
 		},function() {
 			$(this).fadeTo(200, 0.4);
 		}
@@ -51,6 +50,7 @@ $(function(){
 	
 	//上一页按钮
 	$(".pics_switch .pic_prev").click(function() {
+	alert('1');
 		index -= 1;
 		if(index == -1) {index = len - 1;}
 		showPics(index);
