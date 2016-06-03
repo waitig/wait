ce0e5e9f5158e86bdfe838f98a4a77051b681ceb
waitig <waitig@hotmail.com>
2016-06-03 10:40:04 +0800

    修改时间显示问题

--- functions.php
+++ functions.php
@@ -473,7 +473,7 @@ function time_ago( $type = 'commennt', $day = 7 ) {
 }
 function timeago( $ptime ) {
 		$ptime = strtotime($ptime);
-		$etime = time() - $ptime - 8*60*60;
+		$etime = time() - $ptime;
 		if($etime < 1) return '刚刚';
 		$interval = array (
 				12 * 30 * 24 * 60 * 60  =>  '年前 ('.date('Y-m-d', $ptime).')',
