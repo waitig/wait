28d0664affab7090af42ec35f2c47ac5ccd572e6
waitig <waitig@hotmail.com>
2016-06-07 17:05:11 +0800

    version 3.2正式版

--- widgets/wid-author.php
+++ widgets/wid-author.php
@@ -16,8 +16,6 @@ class d_author extends WP_Widget {
 
 		$title = apply_filters('widget_name', $instance['title']);
 		$limit = $instance['limit'];
-		/*$outer = $instance['outer'];
-		$timer = $instance['timer'];*/
 		$addlink = $instance['addlink'];
 		$more = $instance['more'];
 		$link = $instance['link'];
@@ -59,18 +57,6 @@ class d_author extends WP_Widget {
 				<input class="widefat" id="<?php echo $this->get_field_id('limit'); ?>" name="<?php echo $this->get_field_name('limit'); ?>" type="number" value="<?php echo $instance['limit']; ?>" />
 			</label>
 		</p>
-		<!--<p>
-			<label>
-				排除某人：
-				<input class="widefat" id="<?php echo $this->get_field_id('outer'); ?>" name="<?php echo $this->get_field_name('outer'); ?>" type="text" value="<?php echo $instance['outer']; ?>" />
-			</label>
-		</p>
-		<p>
-			<label>
-				几天内：
-				<input class="widefat" id="<?php echo $this->get_field_id('timer'); ?>" name="<?php echo $this->get_field_name('timer'); ?>" type="number" value="<?php echo $instance['timer']; ?>" />
-			</label>
-		</p>-->
 		<p>
 			<label>
 				<input style="vertical-align:-3px;margin-right:4px;" class="checkbox" type="checkbox" <?php checked( $instance['addlink'], 'on' ); ?> id="<?php echo $this->get_field_id('addlink'); ?>" name="<?php echo $this->get_field_name('addlink'); ?>">加链接
@@ -123,7 +109,7 @@ function wait_list_authors( $args = '' ) {
 		$user_email=$author->user_email;
 		$user_link=get_author_posts_url( $author->ID, $author->user_nicename );
 		$user_title=esc_attr( sprintf(__("Posts by %s"), $author->display_name) );
-		$user_avatar='<div class="top_author_avatar">'.str_replace(' src=', ' data-original=', get_avatar( $user_email, $size = '48' , deel_avatar_default())).'</div>';
+		$user_avatar='<div class="top_author_avatar">'.get_avatar( $user_email, $size = '56' , deel_avatar_default()).'</div>';
 		$user_desc=get_the_author_meta('description',$author->ID);
 		$user_label='<span class="label label-'.$i.'">'.$i.'</span>';
 		$posts = isset( $author_count[$author->ID] ) ? $author_count[$author->ID] : 0;
@@ -140,104 +126,4 @@ function wait_list_authors( $args = '' ) {
 	return $output;
 
 };
-function wa_list_authors( $args = '' ) {
-	global $wpdb;
-
-	$defaults = array(
-		'orderby' => 'name', 'order' => 'ASC', 'number' => '',
-		'optioncount' => false, 'exclude_admin' => true,
-		'show_fullname' => false, 'hide_empty' => true,
-		'feed' => '', 'feed_image' => '', 'feed_type' => '', 'echo' => true,
-		'style' => 'list', 'html' => true, 'exclude' => '', 'include' => ''
-	);
-
-	$args = wp_parse_args( $args, $defaults );
-
-	$return = '';
-
-	$query_args = wp_array_slice_assoc( $args, array( 'orderby', 'order', 'number', 'exclude', 'include' ) );
-	$query_args['fields'] = 'ids';
-	$authors = get_users( $query_args );
-
-	$author_count = array();
-	foreach ( (array) $wpdb->get_results( "SELECT DISTINCT post_author, COUNT(ID) AS count FROM $wpdb->posts WHERE " . get_private_posts_cap_sql( 'post' ) . " GROUP BY post_author" ) as $row ) {
-		$author_count[$row->post_author] = $row->count;
-	}
-	foreach ( $authors as $author_id ) {
-		$author = get_userdata( $author_id );
-		$user_email=$author->user_email;
-
-		if ( $args['exclude_admin'] && 'admin' == $author->display_name ) {
-			continue;
-		}
-
-		$posts = isset( $author_count[$author->ID] ) ? $author_count[$author->ID] : 0;
-
-		if ( ! $posts && $args['hide_empty'] ) {
-			continue;
-		}
-
-		if ( $args['show_fullname'] && $author->first_name && $author->last_name ) {
-			$name = "$author->first_name $author->last_name";
-		} else {
-			$name = $author->display_name;
-		}
-
-		if ( ! $args['html'] ) {
-			$return .= $name . ', ';
-
-			continue; // No need to go further to process HTML.
-		}
-
-		if ( 'list' == $args['style'] ) {
-			$return .= '<li>';
-		}
-
-		$link = '<a href="' . get_author_posts_url( $author->ID, $author->user_nicename ) . '" title="' . esc_attr( sprintf(__("Posts by %s"), $author->display_name) ) . '">' . get_avatar( $user_email, $size = '48' , deel_avatar_default() ). $name . '</a>';
-
-		if ( ! empty( $args['feed_image'] ) || ! empty( $args['feed'] ) ) {
-			$link .= ' ';
-			if ( empty( $args['feed_image'] ) ) {
-				$link .= '(';
-			}
-
-			$link .= '<a href="' . get_author_feed_link( $author->ID, $args['feed_type'] ) . '"';
-
-			$alt = '';
-			if ( ! empty( $args['feed'] ) ) {
-				$alt = ' alt="' . esc_attr( $args['feed'] ) . '"';
-				$name = $args['feed'];
-			}
-
-			$link .= '>';
-
-			if ( ! empty( $args['feed_image'] ) ) {
-				$link .= '<img src="' . esc_url( $args['feed_image'] ) . '" style="border: none;"' . $alt . ' />';
-			} else {
-				$link .= $name;
-			}
-
-			$link .= '</a>';
-
-			if ( empty( $args['feed_image'] ) ) {
-				$link .= ')';
-			}
-		}
-
-		if ( $args['optioncount'] ) {
-			$link .= ' ('. $posts . ')';
-		}
-
-		$return .= $link;
-		$return .= ( 'list' == $args['style'] ) ? '</li>' : ', ';
-	}
-
-	$return = rtrim( $return, ', ' );
-
-	if ( ! $args['echo'] ) {
-		return $return;
-	}
-	echo $return;
-}
-
 ?>
